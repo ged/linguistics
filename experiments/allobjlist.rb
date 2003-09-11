e6cd647f65e581de0c71b38c551b017bbe980230
Michael Granger <ged@FaerieMUD.org>
2003-09-11 04:51:41 +0000

    - Added example output.
    --HG--
    extra : convert_revision : svn%3Af906c4d9-f4d7-0310-8a3c-e0469d1b0df1/trunk%4034

--- experiments/allobjlist.rb
+++ experiments/allobjlist.rb
@@ -16,3 +16,19 @@ ObjectSpace::each_object {|obj| allobjs << obj.class.name}
 puts "The current Ruby objectspace contains: " +
 	allobjs.en.conjunction( :generalize => true )
 
+
+# Prints:
+#  "The current Ruby objectspace contains: thousands of Strings, thousands of
+#  Arrays, hundreds of Hashes, hundreds of Classes, many Regexps, a number of
+#  Ranges, a number of Modules, several Files, several Floats, several Procs,
+#  several MatchDatas, several Objects, several IOS, a Binding, a NoMemoryError,
+#  a SystemStackError, a fatal, a Thread, and a ThreadGroup"
+#
+
+# If :generalize is set to 'false', it prints:
+#  "The current Ruby objectspace contains: 8744 Strings, 1025 Arrays, 425
+#  Hashes, 184 Classes, 74 Regexps, 18 Ranges, 18 Modules, five Files, five
+#  Floats, four Procs, three MatchDatas, three Objects, three IOS, a Binding, a
+#  NoMemoryError, a SystemStackError, a fatal, a Thread, and a ThreadGroup"
+#
+
