dbd73fa54277aceb7269937f12b959b250159f7a
Michael Granger <ged@FaerieMUD.org>
2011-01-10 10:36:49 -0800

    Better comments for the klingon example

--- examples/klingon.rb
+++ examples/klingon.rb
@@ -2,11 +2,21 @@
 
 require 'linguistics'
 
-# An example language module that provides Klingon-language inflecton.
+# An example of how you'd start writing a language module that provides
+# Klingon-language inflecton.  It's obviously not really a useful
+# implementation.
+
 module Linguistics::TLH
 
+	# Register the module with the framework
 	Linguistics.register_language( :tlh, self )
 
 end
 
+
+if __FILE__ == $0
+	require 'pp'
+	Linguistics.use( :tlh, :classes => [Object] )
+	pp Object.new.tlh
+end
 
