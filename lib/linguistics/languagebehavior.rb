70c01eedbe615ff72d2219c11f73a0f5d4cc1f8d
Michael Granger <ged@FaerieMUD.org>
2011-01-10 09:56:56 -0800

    Added a RSpec 2 shared behavior for testing language modules

---
+++ lib/linguistics/languagebehavior.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+
+require 'rspec'
+
+require 'linguistics'
+require 'linguistics/iso639'
+
+
+# This is a RSpec 2 shared behavior for language plugins. You can use this to be
+# sure that your language plugin conforms to the API expected by Linguistics. You'll
+# probably want to use it something like this:
+# 
+#    require 'linguistics/languagebehavior'
+#
+#    describe Linguistics::KL do
+#
+#      it_should_behave_like "A Linguistics language module"
+#
+#      # ... any other specs for your module
+#
+#    end
+
+shared_examples_for "a Linguistics language module" do
+
+	let( :language_module ) do
+		described_class
+	end
+
+
+	it "registers itself with the Linguistics module when required" do
+		Linguistics.languages.values.should include( language_module )
+	end
+
+end
+
+
